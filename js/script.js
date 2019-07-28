ede94fd8ac43bc87629e166097e2037da80672c2
Anish Ghimire <anish.ghimire001@gmail.com>
2019-07-29 00:58:12 +0545

    Minor tweaks, slide-in nav, fav-icons etc

--- js/script.js
+++ js/script.js
@@ -1,3 +1,78 @@
+var testimonialWidth = document.getElementById('testimonials-view').offsetWidth;
+var testimonialsWrapper = document.getElementById('testimonials-text');
+var testimonials = document.getElementsByClassName('testimonial');
+var testimonialNumbers = testimonials.length;
+
+testimonialsWrapper.style.width = testimonialWidth * testimonialNumbers + 'px';
+for (var i = 0; i < testimonialNumbers; i++) {
+	testimonials[i].style.width = testimonialWidth + 'px';
+}
+
+
+window.onresize = function() {
+	testimonialWidth = document.getElementById('testimonials-view').offsetWidth;
+	testimonialsWrapper.style.width = testimonialWidth * testimonialNumbers + 'px';
+	for (var i = 0; i < testimonialNumbers; i++) {
+		testimonials[i].style.width = testimonialWidth + 'px';
+	}
+};
+
+
+var left = 0;
+var current = 0;
+var next = 0;
+var dots = document.getElementsByClassName('dot');
+
+function slider(current, next, element, width) {
+	var slide = setInterval( (function() {
+		console.log(current, next);
+		left = left + ((width * (current - next)) / (0.1 * 1000));
+		console.log(left, (next * width));
+		if (((Math.ceil(Math.abs(left))) == (next * width)) || (Math.floor(Math.abs(left))) == (next * width)) {
+			if (((Math.ceil(Math.abs(left))) == (next * width))) {
+				left = Math.ceil(left);
+			} else if ((Math.floor(Math.abs(left))) == (next * width)) {
+				left = Math.floor(left);
+			}
+
+			element.style.marginLeft = left + 'px';
+			clearInterval(slide);
+			initSlider(element, width);
+		}
+		element.style.marginLeft = left + 'px';
+	}), 1);
+}
+
+function initSlider(element, width) {
+	dots[current].classList.add('is-selected');
+
+	console.log(current, next);
+	setTimeout(function() {
+		slider(current, next, element, width);
+	}, 1500);
+
+	dots[current].classList.remove('is-selected');
+	dots[next].classList.add('is-selected');
+
+	current = next;
+	next += 1;
+	if (next >= testimonialNumbers) {
+		next = 0;
+	}
+}
+
+if (window.innerWidth >= 1200){
+	initSlider(testimonialsWrapper, testimonialWidth);
+}
+
+
+
+
+
+
+
+
+
 
 window.onscroll = function() {
 	var currentScroll = window.pageYOffset;
@@ -22,4 +97,13 @@ window.onscroll = function() {
 			black[i].style.display = 'none';
 		}
 	}
+};
+
+
+function openSidebar() {
+	document.getElementById('sidebar').style.width = '100%';
+}
+
+function closeSidebar() {
+	document.getElementById('sidebar').style.width = '0';
 }
